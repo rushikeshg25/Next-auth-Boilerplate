e6b18f1297132a034df8c708ec82d4d1f0ff03bd
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-25 22:52:54 +0530

    Added Middleware

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+export { default } from "next-auth/middleware";
+
+// export const config = { matcher: ["/dashboard"] };
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
